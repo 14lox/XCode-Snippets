a5e2abc4e6bba9f4bcfde2ea76c7edbf166ca93f
David Hovey <dai@14lox.com>
2013-09-16 12:02:17 +1000

    Added property.m

---
+++ property.m
@@ -0,0 +1,9 @@
+// @property declaration
+// Property object/variable declaration
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: All
+// Shortcut: @prop
+
+@property (strong, nonatomic) <#class#> <#name#>;
