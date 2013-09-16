91a849b74d03b752f39e99be66f45131a9d521f4
David Hovey <dai@14lox.com>
2013-09-16 12:10:45 +1000

    Added stringwithformat.m

---
+++ stringwithformat.m
@@ -0,0 +1,8 @@
+// Shortcut for stringWithFormat NSString class format
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+// Shortcut: string
+
+[NSString stringWithFormat:@"%@", <#name#>];
