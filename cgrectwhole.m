14890966da706f236703971159b978e349cf4a5f
David Hovey <dai@14lox.com>
2013-09-17 10:52:29 +1000

    Added cgrectwhole

---
+++ cgrectwhole.m
@@ -0,0 +1,8 @@
+// Makes a CGRect that fills the parent frame
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: All
+// Shortcut: CGRectMake
+
+CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
