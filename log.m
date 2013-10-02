6f0a0eb5445af0370de8bea73800c43faee2d2d5
David Hovey <dai@14lox.com>
2013-10-02 11:32:14 +1000

    Added log.m

---
+++ log.m
@@ -0,0 +1,8 @@
+// Shortcut for NSLog populated with the most regular format
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+// Shortcut: log
+
+ NSLog(@"<#statment#> = %@", <#variable#>);
